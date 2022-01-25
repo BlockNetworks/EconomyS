0db71af5fe9d4fe05d6fc47fd938e82ce61e9b9b
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2022-01-25 13:01:21 -0800

    Update MyMoneyCommand.php

--- EconomyAPI/src/onebone/economyapi/command/MyMoneyCommand.php
+++ EconomyAPI/src/onebone/economyapi/command/MyMoneyCommand.php
@@ -11,29 +11,29 @@ use onebone\economyapi\EconomyAPI;
 
 class MyMoneyCommand extends Command {
 
-	public function __construct(EconomyAPI $plugin) {
-		$desc = $plugin->getCommandMessage("mymoney");
-		parent::__construct("mymoney", $desc["description"], $desc["usage"]);
+    public function __construct(EconomyAPI $plugin) {
+        $desc = $plugin->getCommandMessage("mymoney");
+        parent::__construct("mymoney", $desc["description"], $desc["usage"]);
 
-		$this->setPermission("economyapi.command.mymoney");
+        $this->setPermission("economyapi.command.mymoney");
 
-		$this->plugin = $plugin;
-	}
+        $this->plugin = $plugin;
+    }
 
-	public function execute(CommandSender $sender, string $label, array $params): bool {
-		if (!$this->plugin->isEnabled()) {
-			return false;
-		}
-		if (!$this->testPermission($sender)) {
-			return false;
-		}
+    public function execute(CommandSender $sender, string $label, array $params): bool {
+        if (!$this->plugin->isEnabled()) {
+            return false;
+        }
+        if (!$this->testPermission($sender)) {
+            return false;
+        }
 
-		if ($sender instanceof Player) {
-			$money = $this->plugin->myMoney($sender);
-			$sender->sendMessage($this->plugin->getMessage("mymoney-mymoney", [$money]));
-			return true;
-		}
-		$sender->sendMessage(TextFormat::RED."Please run this command in-game.");
-		return true;
-	}
+        if ($sender instanceof Player) {
+            $money = $this->plugin->myMoney($sender);
+            $sender->sendMessage($this->plugin->getMessage("mymoney-mymoney", [$money]));
+            return true;
+        }
+        $sender->sendMessage(TextFormat::RED."Please run this command in-game.");
+        return true;
+    }
 }
