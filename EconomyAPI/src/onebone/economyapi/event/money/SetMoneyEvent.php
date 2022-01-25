ab7243cf5d3bb34636174140389453a4d4f748f1
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2022-01-25 13:06:53 -0800

    Update SetMoneyEvent.php

--- EconomyAPI/src/onebone/economyapi/event/money/SetMoneyEvent.php
+++ EconomyAPI/src/onebone/economyapi/event/money/SetMoneyEvent.php
@@ -24,24 +24,21 @@ use onebone\economyapi\event\EconomyAPIEvent;
 
 use onebone\economyapi\EconomyAPI;
 
-class SetMoneyEvent extends EconomyAPIEvent
-{
+class SetMoneyEvent extends EconomyAPIEvent {
+
     public static $handlerList;
 
-    public function __construct(EconomyAPI $plugin, private $username, private $money, $issuer)
-    {
+    public function __construct(EconomyAPI $plugin, private $username, private $money, $issuer) {
         parent::__construct($plugin, $issuer);
         $this->username = $username;
         $this->money = $money;
     }
 
-    public function getUsername()
-    {
+    public function getUsername() {
         return $this->username;
     }
 
-    public function getMoney()
-    {
+    public function getMoney() {
         return $this->money;
     }
 }
