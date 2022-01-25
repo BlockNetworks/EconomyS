2447a6618f8a88edf95acc195d9f8355a38f6d4e
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2022-01-25 13:39:16 -0800

    Update MySQLProvider.php

--- EconomyAPI/src/onebone/economyapi/provider/MySQLProvider.php
+++ EconomyAPI/src/onebone/economyapi/provider/MySQLProvider.php
@@ -20,185 +20,185 @@
 
 namespace onebone\economyapi\provider;
 
-
 use onebone\economyapi\EconomyAPI;
 use onebone\economyapi\task\MySQLPingTask;
 
 use pocketmine\player\Player;
 
-class MySQLProvider implements Provider{
-	/**
-	 * @var \mysqli
-	 */
-	private $db;
+class MySQLProvider implements Provider {
 
-	/** @var EconomyAPI */
-	public function __construct(private EconomyAPI $plugin){
-		$this->plugin = $plugin;
-	}
+    private $db;
 
-	public function open(){
-		$config = $this->plugin->getConfig()->get("provider-settings", []);
+    public function __construct(private EconomyAPI $plugin) {
+        $this->plugin = $plugin;
+    }
 
-		$this->db = new \mysqli(
-			$config["host"] ?? "127.0.0.1",
-			$config["user"] ?? "onebone",
-			$config["password"] ?? "hello_world",
-			$config["db"] ?? "economyapi",
-			$config["port"] ?? 3306);
-		if($this->db->connect_error){
-			$this->plugin->getLogger()->critical("Could not connect to MySQL server: ".$this->db->connect_error);
-			return;
-		}
-		if(!$this->db->query("CREATE TABLE IF NOT EXISTS user_money(
-			username VARCHAR(20) PRIMARY KEY,
-			money FLOAT
-		);")){
-			$this->plugin->getLogger()->critical("Error creating table: " . $this->db->error);
-			return;
-		}
+    public function open() {
+        $config = $this->plugin->getConfig()->get("provider-settings", []);
 
-		$this->plugin->getScheduler()->scheduleRepeatingTask(new MySQLPingTask($this->plugin, $this->db), 600);
-	}
+        $this->db = new \mysqli($config["host"] ?? "127.0.0.1",
+                                $config["user"] ?? "onebone",
+                                $config["password"] ?? "hello_world",
+                                $config["db"] ?? "economyapi",
+                                $config["port"] ?? 3306);
+        if ($this->db->connect_error){
+            $this->plugin->getLogger()->critical("Could not connect to MySQL server: ".$this->db->connect_error);
+            return;
+        }
+        if (!$this->db->query("CREATE TABLE IF NOT EXISTS user_money(
+            username VARCHAR(20) PRIMARY KEY,
+            money FLOAT
+        );")) {
+            $this->plugin->getLogger()->critical("Error creating table: " . $this->db->error);
+            return;
+        }
 
-	/**
-	 * @param \pocketmine\player\Player|string $player
-	 * @return bool
-	 */
-	public function accountExists($player){
-		if($player instanceof Player){
-			$player = $player->getName();
-		}
-		$player = strtolower($player);
+        $this->plugin->getScheduler()->scheduleRepeatingTask(new MySQLPingTask($this->plugin, $this->db), 600);
+    }
 
-		$result = $this->db->query("SELECT * FROM user_money WHERE username='".$this->db->real_escape_string($player)."'");
-		return $result->num_rows > 0 ? true:false;
-	}
+    /**
+     * @param \pocketmine\player\Player|string $player
+     * @return bool
+     */
+    public function accountExists($player) {
+        if ($player instanceof Player) {
+            $player = $player->getName();
+        }
+        $player = strtolower($player);
 
-	/**
-	 * @param \pocketmine\player\Player|string $player
-	 * @param float $defaultMoney
-	 * @return bool
-	 */
-	public function createAccount($player, $defaultMoney = 1000.0){
-		if($player instanceof Player){
-			$player = $player->getName();
-		}
-		$player = strtolower($player);
+        $result = $this->db->query("SELECT * FROM user_money WHERE username='".$this->db->real_escape_string($player)."'");
+        return $result->num_rows > 0 ? true:false;
+    }
 
-		if(!$this->accountExists($player)){
-			$this->db->query("INSERT INTO user_money (username, money) VALUES ('".$this->db->real_escape_string($player)."', $defaultMoney);");
-			return true;
-		}
-		return false;
-	}
+    /**
+     * @param \pocketmine\player\Player|string $player
+     * @param float $defaultMoney
+     * @return bool
+     */
+    public function createAccount($player, $defaultMoney = 1000.0) {
+        if ($player instanceof Player) {
+            $player = $player->getName();
+        }
+        $player = strtolower($player);
 
-	/**
-	 * @param \pocketmine\player\Player|string $player
-	 * @return bool
-	 */
-	public function removeAccount($player){
-		if($player instanceof Player){
-			$player = $player->getName();
-		}
-		$player = strtolower($player);
+        if (!$this->accountExists($player)) {
+            $this->db->query("INSERT INTO user_money (username, money) VALUES ('".$this->db->real_escape_string($player)."', $defaultMoney);");
+            return true;
+        }
+        return false;
+    }
 
-		if($this->db->query("DELETE FROM user_money WHERE username='".$this->db->real_escape_string($player)."'") === true) return true;
-		return false;
-	}
+    /**
+     * @param \pocketmine\player\Player|string $player
+     * @return bool
+     */
+    public function removeAccount($player) {
+        if ($player instanceof Player) {
+            $player = $player->getName();
+        }
+        $player = strtolower($player);
 
-	/**
-	 * @param string $player
-	 * @return float|bool
-	 */
-	public function getMoney($player){
-		if($player instanceof Player){
-			$player = $player->getName();
-		}
-		$player = strtolower($player);
+        if ($this->db->query("DELETE FROM user_money WHERE username='" . $this->db->real_escape_string($player) . "'") === true) {
+            return true;
+        }
+        return false;
+    }
 
-		$res = $this->db->query("SELECT money FROM user_money WHERE username='".$this->db->real_escape_string($player)."'");
-		$ret = $res->fetch_array()[0] ?? false;
-		$res->free();
-		return $ret;
-	}
+    /**
+     * @param string $player
+     * @return float|bool
+     */
+    public function getMoney($player) {
+        if ($player instanceof Player) {
+            $player = $player->getName();
+        }
+        $player = strtolower($player);
 
-	/**
-	 * @param \pocketmine\player\Player|string $player
-	 * @param float $amount
-	 * @return bool
-	 */
-	public function setMoney($player, $amount){
-		if($player instanceof Player){
-			$player = $player->getName();
-		}
-		$player = strtolower($player);
+        $res = $this->db->query("SELECT money FROM user_money WHERE username='".$this->db->real_escape_string($player)."'");
+        $ret = $res->fetch_array()[0] ?? false;
+        $res->free();
+        return $ret;
+    }
 
-		$amount = (float) $amount;
+    /**
+     * @param \pocketmine\player\Player|string $player
+     * @param float $amount
+     * @return bool
+     */
+    public function setMoney($player, $amount) {
+        if ($player instanceof Player) {
+            $player = $player->getName();
+        }
+        $player = strtolower($player);
 
-		return $this->db->query("UPDATE user_money SET money = $amount WHERE username='".$this->db->real_escape_string($player)."'");
-	}
+        $amount = (float) $amount;
 
-	/**
-	 * @param \pocketmine\player\Player|string $player
-	 * @param float $amount
-	 * @return bool
-	 */
-	public function addMoney($player, $amount){
-		if($player instanceof Player){
-			$player = $player->getName();
-		}
-		$player = strtolower($player);
+        return $this->db->query("UPDATE user_money SET money = $amount WHERE username='".$this->db->real_escape_string($player)."'");
+    }
 
-		$amount = (float) $amount;
+    /**
+     * @param \pocketmine\player\Player|string $player
+     * @param float $amount
+     * @return bool
+     */
+    public function addMoney($player, $amount) {
+        if ($player instanceof Player) {
+            $player = $player->getName();
+        }
+        $player = strtolower($player);
 
-		return $this->db->query("UPDATE user_money SET money = money + $amount WHERE username='".$this->db->real_escape_string($player)."'");
-	}
+        $amount = (float) $amount;
 
-	/**
-	 * @param \pocketmine\player\Player|string $player
-	 * @param float $amount
-	 * @return bool
-	 */
-	public function reduceMoney($player, $amount){
-		if($player instanceof Player){
-			$player = $player->getName();
-		}
-		$player = strtolower($player);
+        return $this->db->query("UPDATE user_money SET money = money + $amount WHERE username='".$this->db->real_escape_string($player)."'");
+    }
 
-		$amount = (float) $amount;
+    /**
+     * @param \pocketmine\player\Player|string $player
+     * @param float $amount
+     * @return bool
+     */
+    public function reduceMoney($player, $amount) {
+        if ($player instanceof Player) {
+            $player = $player->getName();
+        }
+        $player = strtolower($player);
 
-		return $this->db->query("UPDATE user_money SET money = money - $amount WHERE username='".$this->db->real_escape_string($player)."'");
-	}
+        $amount = (float) $amount;
 
-	/**
-	 * @return array
-	 */
-	public function getAll(){
-		$res = $this->db->query("SELECT * FROM user_money");
+        return $this->db->query("UPDATE user_money SET money = money - $amount WHERE username='".$this->db->real_escape_string($player)."'");
+    }
 
-		$ret = [];
-		foreach($res->fetch_all() as $val){
-			$ret[$val[0]] = $val[1];
-		}
+    /**
+     * @return array
+     */
+    public function getAll() {
+        $res = $this->db->query("SELECT * FROM user_money");
 
-		$res->free();
+        $ret = [];
+        foreach ($res->fetch_all() as $val) {
+            $ret[$val[0]] = $val[1];
+        }
 
-		return $ret;
-	}
+        $res->free();
 
-	/**
-	 * @return string
-	 */
-	public function getName(){
-		return "MySQL";
-	}
+        return $ret;
+    }
 
-	public function save(){}
+    /**
+     * @return string
+     */
+    public function getName() {
+        return "MySQL";
+    }
 
-	public function close(){
-		if($this->db instanceof \mysqli){
-			$this->db->close();
-		}
-	}
+    public function save() {
+        //
+    }
+
+    public function close() {
+        if ($this->db instanceof \mysqli) {
+            $this->db->close();
+        }
+    }
+
 }
